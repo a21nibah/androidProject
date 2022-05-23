a951c46a0f1dcddb86e330e9750a0de4aa96240e
a21nibah <a21nibah@student.his.se>
2022-05-24 00:37:14 +0200

    added the ability to launch the about activity from the main activity

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button aboutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutBtn=findViewById(R.id.launch_about_activity_btn);
+        Intent intent=new Intent(this, AboutActivity.class);
+        aboutBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+            startActivity(intent);
+            }
+        });
+        
     }
 }
