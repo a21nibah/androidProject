a8d80135df5ddfb12ef722e6372f828255cb9992
a21nibah <a21nibah@student.his.se>
2022-05-24 22:56:15 +0200

    added text views to the carViewHOlder

--- app/src/main/java/com/example/androidproject/CarAdapter.java
+++ app/src/main/java/com/example/androidproject/CarAdapter.java
@@ -2,12 +2,20 @@ package com.example.androidproject;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder>
 {
+private ArrayList<Car> cars;
+
+    public CarAdapter(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
 
     @NonNull
     @Override
@@ -27,9 +35,14 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder>
 
     public class CarViewHolder extends RecyclerView.ViewHolder
 {
-
+private TextView name;
+private TextView company;
+private TextView category;
     public CarViewHolder(@NonNull View itemView) {
         super(itemView);
+this.name=itemView.findViewById(R.id.name);
+                this.category=itemView.findViewById(R.id.category);
+                this.company=itemView.findViewById(R.id.company);
     }
 }
 }
