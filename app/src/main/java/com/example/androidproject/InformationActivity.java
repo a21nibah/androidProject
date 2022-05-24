d3453d0bc15edae06365e0e6fd171a1c5f6ddddf
a21nibah <a21nibah@student.his.se>
2022-05-24 23:34:47 +0200

    added some member variables to the info activity and a recycler view as well to the same activity

--- app/src/main/java/com/example/androidproject/InformationActivity.java
+++ app/src/main/java/com/example/androidproject/InformationActivity.java
@@ -1,15 +1,28 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 @SuppressWarnings("FieldCanBeLocal")
 
 public class InformationActivity extends AppCompatActivity {
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
+    private ArrayList<Car> cars;
+private Gson gson;
+private Type type;
+private RecyclerView recView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        recView=findViewById(R.id.rec)
     }
 }
